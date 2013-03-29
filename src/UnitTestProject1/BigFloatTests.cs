eee1933b414f94c3d95365dbc128403214618ef6
Fabian Kruse <fabian.kruse@a2-c.ch>
2013-03-29 15:40:04 +0100

    BigFloat implementiert

---
+++ src/UnitTestProject1/BigFloatTests.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ZHAW.SoftwareProjekt.Fractals.DataStructures
+{
+    [TestClass]
+    public class BigFloatTests
+    {
+
+        [TestMethod]
+        public void ZuweisungsTests()
+        {
+            BigFloat bd7 = "10230.1020300";
+            Assert.AreEqual("10230.10203", bd7.ToString(), "0 Zuweisung per String --> fehlerhaft");
+
+            BigFloat bd1 = 123.456m;
+            Assert.AreEqual("123.456", bd1.ToString(), "Zuweisung per Dezimal --> fehlerhaft");
+
+            BigFloat bd2 = "123.456";
+            Assert.AreEqual("123.456", bd2.ToString(), "Zuweisung per String --> fehlerhaft");
+
+            BigFloat bd3 = "-123.456";
+            Assert.AreEqual("-123.456", bd3.ToString(), "Zuweisung per String Negativ --> fehlerhaft");
+
+            BigFloat bd4 = -123.456m;
+            Assert.AreEqual("-123.456", bd4.ToString(), "Zuweisung per Dezimal Negativ --> fehlerhaft");
+
+            BigFloat bd5 = 0;
+            Assert.AreEqual("0", bd5.ToString(), "0 Zuweisung per Dezimal --> fehlerhaft");
+
+            BigFloat bd6 = 0;
+            Assert.AreEqual("0", bd6.ToString(), "0 Zuweisung per String --> fehlerhaft");
+
+        }
+
+        [TestMethod]
+        public void AdditionSubtraktionsTests()
+        {
+            BigFloat bd1 = 123.123m;
+            BigFloat bd2 = 123.123m;
+            BigFloat r = bd1 + bd2;
+            Assert.AreEqual("246.246", r.ToString(), "Addition ohne Überlauf --> fehlerhaft");
+
+            bd1 = 123.123m;
+            bd2 = 123.123m;
+            r = bd1 - bd2;
+            Assert.AreEqual("0", r.ToString(), "Subtraktion ohne Überlauf --> fehlerhaft");
+
+            bd1 = 123.456m;
+            bd2 = 123.456m;
+            r = bd1 + bd2;
+            Assert.AreEqual("246.912", r.ToString(), "Addition mit Überlauf nach dem Dezimalpunkt --> fehlerhaft");
+
+            bd1 = 456.456m;
+            bd2 = 456.456m;
+            r = bd1 + bd2;
+            Assert.AreEqual("912.912", r.ToString(), "Addition mit Überlauf nach und vor dem Dezimalpunkt --> fehlerhaft");
+
+            bd1 = 456.789m;
+            bd2 = 456.789m;
+            r = bd1 + bd2;
+            Assert.AreEqual("913.578", r.ToString(), "Addition mit Überlauf nach und vor dem Dezimalpunkt und über den Dezimalpunkt --> fehlerhaft");
+
+            bd1 = 879056709.213256854m;
+            bd2 = 1447.987654321m;
+            r = bd1 + bd2;
+            Assert.AreEqual("879058157.200911175", r.ToString(), "Komplexe Addition 1 --> fehlerhaft");
+
+            bd1 = -765.924m;
+            bd2 = 1447.4098m;
+            r = bd1 + bd2;
+            Assert.AreEqual("681.4858", r.ToString(), "Komplexe Addition 2 --> fehlerhaft");
+
+            bd1 = 765.924m;
+            bd2 = -1447.4098m;
+            r = bd1 + bd2;
+            Assert.AreEqual("-681.4858", r.ToString(), "Komplexe Addition 3 --> fehlerhaft");
+
+            bd1 = 765.924m;
+            bd2 = -1447.4098m;
+            r = bd1 - bd2;
+            Assert.AreEqual("2213.3338", r.ToString(), "Komplexe Subtraktion 1 --> fehlerhaft");
+        }
+
+        [TestMethod]
+        public void KombinierteTests()
+        {
+            BigFloat bd1 = 12.88m;
+            BigFloat bd2 = 123.456m;
+            BigFloat r = (bd1 + bd2) * (bd1 - bd2);
+            Assert.AreEqual("-15075.489536", r.ToString(), "Kombinierte 1 --> fehlerhaft");
+
+            bd1 = 12.88m;
+            bd2 = -1;
+            r = bd1 * bd2;
+            Assert.AreEqual("-12.88", r.ToString(), "Kombinierte 2 --> fehlerhaft");
+
+            bd1 = 123.123m;
+            bd2 = 923.529123m;
+            BigFloat r1 = bd1 + bd2;
+            BigFloat r2 = r1 + bd2;
+            BigFloat r3 = r2 + bd2;
+            BigFloat r4 = r3 - bd2;
+            BigFloat r5 = r4 - bd2;
+            BigFloat r6 = r5 - bd2;
+            Assert.AreEqual("123.123", r6.ToString(), "Kombinierte 3 --> fehlerhaft");
+
+            bd1 = 12356789.987654321m;
+            r = (bd1 * bd1 * bd1 * bd1);
+            r = (r / bd1 / bd1 / bd1);
+            Assert.AreEqual("12356789.987654321", r.ToString(), "Kombinierte 4 --> fehlerhaft");
+        }
+
+    }
+}
---
+++ src/ZHAW.SoftwareProjekt.Fractals.DataStructures/BigFloat.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+using System.Globalization;
+
+namespace ZHAW.SoftwareProjekt.Fractals.DataStructures
+{
+    public struct BigFloat
+    {
+        public BigInteger _Value { get; set; }          // Beinhaltet in einem beliebiggrossen Array den Wert der Zahl
+        public BigInteger _Skalierung { get; set; }     // Wo befindet sich der Dezimalpunkt von Rechts aus
+
+        public BigFloat(BigInteger Value, BigInteger Skalierung): this()
+        {
+            _Value = Value;
+            _Skalierung = Skalierung;
+            while (Skalierung > 0 && Value % 10 == 0)
+            {
+                Value /= 10;
+                Skalierung -= 1;
+            }
+        }
+
+        public static implicit operator BigFloat(decimal Dezimal) // Zuweisung mit "=", Achtung "m" nicht vergessen !!! (134.12345m)
+        {
+            BigInteger value = (BigInteger)Dezimal;
+            BigInteger skalierung = 0;
+            decimal faktor = 1m;
+            while ((decimal)value != Dezimal * faktor)
+            {
+                skalierung += 1;
+                faktor *= 10;
+                value = (BigInteger)(Dezimal * faktor);
+            }
+            return new BigFloat(value, skalierung);
+        }
+
+        public static implicit operator BigFloat(string Dezimal) // Zuweisung mit "=", als String
+        {
+            BigInteger skalierung = 0;
+            if (Dezimal.Contains("."))
+            {
+                char[] charsToTrim = {'0'};
+
+                Dezimal = Dezimal.TrimEnd(charsToTrim);
+                skalierung = (Dezimal.Length - Dezimal.IndexOf(".")) - 1;
+                Dezimal = Dezimal.Replace(".", "");
+            }
+            BigInteger value = BigInteger.Parse(Dezimal);
+            return new BigFloat(value, skalierung);
+        }
+
+        // Rechenoperationen -->
+        public static BigFloat operator +(BigFloat BigFloat1, BigFloat BigFloat2) // Addition
+        {
+            int SkalierungsDifferenz = (int)(BigFloat1._Skalierung - BigFloat2._Skalierung);
+            BigInteger neueSkalierung = BigFloat1._Skalierung;
+            if (SkalierungsDifferenz > 0)
+            {
+                BigFloat2._Value = BigFloat2._Value * BigInteger.Pow(10, SkalierungsDifferenz);
+                neueSkalierung = BigFloat1._Skalierung;
+            }
+            else if (SkalierungsDifferenz <= 0)
+            {
+                BigFloat1._Value = BigFloat1._Value * BigInteger.Pow(10, Math.Abs(SkalierungsDifferenz));
+                neueSkalierung = BigFloat2._Skalierung;
+            }
+            return Norm(new BigFloat(BigFloat1._Value + BigFloat2._Value, neueSkalierung));
+        }
+
+        public static BigFloat operator -(BigFloat BigFloat1, BigFloat BigFloat2) // Subtraktion
+        {
+            int SkalierungsDifferenz = (int)(BigFloat1._Skalierung - BigFloat2._Skalierung);
+            BigInteger neueSkalierung = BigFloat1._Skalierung;
+            if (SkalierungsDifferenz > 0)
+            {
+                BigFloat2._Value = BigFloat2._Value * BigInteger.Pow(10, SkalierungsDifferenz);
+                neueSkalierung = BigFloat1._Skalierung;
+            }
+            else if (SkalierungsDifferenz <= 0)
+            {
+                BigFloat1._Value = BigFloat1._Value * BigInteger.Pow(10, Math.Abs(SkalierungsDifferenz));
+                neueSkalierung = BigFloat2._Skalierung;
+            }
+            return Norm(new BigFloat(BigFloat1._Value - BigFloat2._Value, neueSkalierung));
+        }
+
+        public static BigFloat operator *(BigFloat BigFloat1, BigFloat BigFloat2) // Multiplikation
+        {
+            return new BigFloat(BigFloat1._Value * BigFloat2._Value, BigFloat1._Skalierung + BigFloat2._Skalierung);
+        }
+
+        public static BigFloat operator /(BigFloat BigFloat1, BigFloat BigFloat2) // Division
+        {
+            return new BigFloat(BigFloat1._Value / BigFloat2._Value, BigFloat1._Skalierung - BigFloat2._Skalierung);
+        }
+        // <-- Rechenoperationen
+
+        public override string ToString()
+        {
+            string s = _Value.ToString();
+            if (_Skalierung != 0)
+            {
+                if (_Skalierung > Int32.MaxValue) return "[Zu Grosse Skalierung --> Kann nicht angeziegt werden]";
+                int Dezimalpunkt = s.Length - (int)_Skalierung;
+                s = s.Insert(Dezimalpunkt, Dezimalpunkt == 0 ? "0." : ".");
+            }
+            return s;
+        }
+
+        private static BigFloat Norm(BigFloat BigFloat1)
+        {
+            if (BigFloat1._Value == 0)
+            {
+                BigFloat1._Skalierung = 0;
+            }
+            BigFloat BigFloat2 = BigFloat1.ToString();
+            return BigFloat2;
+        }
+    }
+}
