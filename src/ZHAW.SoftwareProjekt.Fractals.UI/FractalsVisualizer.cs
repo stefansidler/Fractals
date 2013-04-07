c4d5683236e4ff203d90e9a477b2c0b8e890521b
Fabian Kruse <fabian.kruse@a2-c.ch>
2013-04-07 16:39:58 +0200

    Zeichnungsgrösse verändert

--- src/ZHAW.SoftwareProjekt.Fractals.UI/FractalsVisualizer.cs
+++ src/ZHAW.SoftwareProjekt.Fractals.UI/FractalsVisualizer.cs
@@ -30,17 +30,35 @@ namespace ZHAW.SoftwareProjekt.Fractals.UI
         private void LoadFractals()
         {
             fractalsList.DisplayMember = "Name";
-            fractalsList.Items.Add(new Mandelbrot { Name = "Mandelbrot - standard", Xmin = -2.1, Xmax = 1.0, Ymin = -1.3, Ymax = 1.3 });
+            fractalsList.Items.Add(new Mandelbrot { Name = "Mandelbrot - standard 1", Xmin = -2.1, Xmax = 1.0, Ymin = -1.3, Ymax = 1.3 });
+            fractalsList.Items.Add(new Mandelbrot { Name = "Mandelbrot - standard 2", Xmin = -1.41625852044, Xmax = -1.34883934224, Ymin = 0.0121250642032, Ymax = 0.0194777201008 });
+            fractalsList.Items.Add(new Mandelbrot { Name = "Mandelbrot - standard 3", Xmin = -1.29746953237867, Xmax = -1.29368412662443, Ymin = -0.441966505864, Ymax = -0.4416893457808 });
             fractalsList.Items.Add(new Mandelbrot { Name = "Mandelbrot - View 1", Xmin = -0.8, Xmax = -0.7, Ymin = 0.1, Ymax = 0.2 });
             fractalsList.Items.Add(new Mandelbrot { Name = "Mandelbrot - View 2", Xmin = -0.795, Xmax = -0.78, Ymin = 0.138, Ymax = 0.15 });
             fractalsList.Items.Add(new MandelbrotBigFloat
                 {
-                    Name = "BigFloat Mandelbrot",
+                    Name = "BigFloat Mandelbrot 1",
                     Xmin = -2.1m,
                     Xmax = 1.0m,
                     Ymin = -1.3m,
                     Ymax = 1.3m
                 });
+            fractalsList.Items.Add(new MandelbrotBigFloat
+            {
+                Name = "BigFloat Mandelbrot 2",
+                Xmin = -1.41625852044m,
+                Xmax = -1.34883934224m,
+                Ymin = 0.0121250642032m,
+                Ymax = 0.0194777201008m
+            });
+            fractalsList.Items.Add(new MandelbrotBigFloat
+            {
+                Name = "BigFloat Mandelbrot 3",
+                Xmin = -1.29746953237867m,
+                Xmax = -1.29368412662443m,
+                Ymin = -0.441966505864m,
+                Ymax = -0.4416893457808m
+            });
 
             fractalsList.SelectedIndex = 0;
         }
